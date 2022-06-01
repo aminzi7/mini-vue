22627640b9dddc8172a10ede8b7c245b3f69caf6
Aminzy <d1243410203@163.com>
2022-06-01 17:44:29 +0800

    component happy path

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,44 @@
+export function createComponentInstance (vnode) {
+  const component = {
+    vnode,
+    type: vnode.type
+  }
+
+  return component
+}
+
+export function setupComponent (instance) {
+  // init props
+  // init solt
+  // 处理setup 返回值 函数？ 对象？
+  setupStatefulComponent(instance)
+}
+
+function setupStatefulComponent (instance: any) {
+  // get state
+  const Component = instance.type
+
+  const { setup } = Component
+  if (setup) {
+    const setupResult = setup()
+
+    handleSetupResult(instance, setupResult)
+  }
+}
+
+function handleSetupResult (instance, setupResult: any) {
+  // 先对象
+  if (typeof setupResult === 'object') {
+    instance.setupState = setupResult
+  }
+
+  finishComponentSetup(instance)
+}
+
+function finishComponentSetup (instance: any) {
+  const Component = instance.type
+
+  if (Component.render) {
+    instance.render = Component.render
+  }
+}
---
+++ src/runtime-core/renderer.ts
@@ -0,0 +1,28 @@
+import { createComponentInstance, setupComponent } from './component'
+
+export function render (vnode, container) {
+  // path 后面的递归
+  path(vnode, container)
+}
+
+function path (vnode, container) {
+  // 判断是不是element
+  processComponent(vnode, container)
+}
+
+function processComponent (vnode: any, container: any) {
+  mountComponent(vnode, container)
+}
+
+function mountComponent (vnode: any, container) {
+  const instance = createComponentInstance(vnode)
+
+  setupComponent(instance)
+  setupRenderEffect(instance, container)
+}
+
+function setupRenderEffect (instance, container) {
+  const subTree = instance.render()
+  // vnode is element -> mountelement
+  path(subTree, container)
+}
