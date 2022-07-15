0e2b6ae2249998179ba54cab64748a26b23a446a
Aminzy <d1243410203@163.com>
2022-07-15 12:01:44 +0800

    更新element props

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,4 @@
-import { isObject } from '../shared/index'
+import { EMPTY_OBJ, isObject } from '../shared/index'
 import { ShapeFlags } from '../shared/ShapeFlags'
 import { createComponentInstance, setupComponent } from './component'
 import { Fragment, Text } from './vnode'
@@ -67,6 +67,33 @@ export function createRenderer (options) {
     console.log('pathElement')
     console.log('n1', n1)
     console.log('n2', n2)
+    const oldProps = n1.props || EMPTY_OBJ
+    const newProps = n2.props || EMPTY_OBJ
+
+    const el = (n2.el = n1.el)
+
+    patchProps(el, oldProps, newProps)
+  }
+
+  function patchProps (el, oldProps: any, newProps: any) {
+    if (oldProps !== newProps) {
+      for (const key in newProps) {
+        const prevProp = oldProps[key]
+        const nextProp = newProps[key]
+
+        if (prevProp !== nextProp) {
+          hostPatchProp(el, key, prevProp, nextProp)
+        }
+      }
+
+      if (oldProps !== EMPTY_OBJ) {
+        for (const key in oldProps) {
+          if (!(key in newProps)) {
+            hostPatchProp(el, key, oldProps[key], null)
+          }
+        }
+      }
+    }
   }
   // 初始化
   function mountElement (vnode: any, container: any, parentComponent) {
@@ -92,7 +119,7 @@ export function createRenderer (options) {
     const { props } = vnode
     for (const key in props) {
       const val = props[key]
-      hostPatchProp(el, key, val)
+      hostPatchProp(el, key, null, val)
     }
     // canvs
     // el.x = 10
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -3,6 +3,7 @@ export const extend = Object.assign
 export const isObject = value => {
   return value !== null && typeof value === 'object'
 }
+export const EMPTY_OBJ = {}
 
 export const hasChanged = (val, newValue) => {
   return !Object.is(val, newValue)
