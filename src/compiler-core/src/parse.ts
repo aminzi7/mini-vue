2359d331008cbf748a2fd323f096099dd86f2b8f
Aminzy <d1243410203@163.com>
2022-07-23 10:34:16 +0800

    解析element标签

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,64 +1,97 @@
-import { NodeTypes } from "./ast";
+import { NodeTypes } from './ast'
 
-export function baseParse(content: string) {
-  const context = createParserContext(content);
-  return createRoot(parseChildren(context));
+const enum TagType {
+  Start,
+  End
 }
 
-function parseChildren(context) {
-  const nodes: any = [];
+export function baseParse (content: string) {
+  const context = createParserContext(content)
+  return createRoot(parseChildren(context))
+}
 
-  let node;
-  if (context.source.startsWith("{{")) {
-     node = parseInterpolation(context);
+function parseChildren (context) {
+  const nodes: any = []
+
+  let node
+  const s = context.source
+  if (s.startsWith('{{')) {
+    node = parseInterpolation(context)
+  } else if (s[0] === '<') {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context)
+    }
   }
 
-  nodes.push(node);
+  nodes.push(node)
 
-  return nodes;
+  return nodes
 }
 
-function parseInterpolation(context) {
+function parseElement (context: any) {
+  const element = parseTag(context, TagType.Start)
+
+  parseTag(context, TagType.End)
+
+  return element
+}
+
+function parseTag (context: any, type: TagType) {
+  // <div></div>
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+  const tag = match[1]
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+
+  if (type === TagType.End) return
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag
+  }
+}
+
+function parseInterpolation (context) {
   // {{message}}
 
-  const openDelimiter = "{{";
-  const closeDelimiter = "}}";
+  const openDelimiter = '{{'
+  const closeDelimiter = '}}'
 
   const closeIndex = context.source.indexOf(
     closeDelimiter,
     openDelimiter.length
-  );
+  )
 
-  advanceBy(context, openDelimiter.length);
+  advanceBy(context, openDelimiter.length)
 
-  const rawContentLength = closeIndex - openDelimiter.length;
+  const rawContentLength = closeIndex - openDelimiter.length
 
-  const rawContent = context.source.slice(0, rawContentLength);
+  const rawContent = context.source.slice(0, rawContentLength)
   const content = rawContent.trim()
 
-  advanceBy(context, rawContentLength + closeDelimiter.length);
+  advanceBy(context, rawContentLength + closeDelimiter.length)
 
   return {
     type: NodeTypes.INTERPOLATION,
     content: {
       type: NodeTypes.SIMPLE_EXPRESSION,
-      content: content,
-    },
-  };
+      content: content
+    }
+  }
 }
 
-function advanceBy(context: any, length: number) {
-  context.source = context.source.slice(length);
+function advanceBy (context: any, length: number) {
+  context.source = context.source.slice(length)
 }
 
-function createRoot(children) {
+function createRoot (children) {
   return {
-    children,
-  };
+    children
+  }
 }
 
-function createParserContext(content: string): any {
+function createParserContext (content: string): any {
   return {
-    source: content,
-  };
+    source: content
+  }
 }
