5a0f4b3d30c76084d4f317f52943bddfc0731038
Aminzy <d1243410203@163.com>
2022-07-27 23:03:37 +0800

    string类型

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,33 +1,38 @@
-export function transform(root, options) {
-  const context = createTransformContext(root, options);
-  traverseNode(root, context);
+export function transform (root, options = {}) {
+  const context = createTransformContext(root, options)
+  traverseNode(root, context)
+  createRootCodegen(root)
 }
 
-function createTransformContext(root: any, options: any): any {
+function createRootCodegen (root: any) {
+  root.codegenNode = root.children[0]
+}
+
+function createTransformContext (root: any, options: any): any {
   const context = {
     root,
-    nodeTransforms: options.nodeTransforms || [],
-  };
-
-  return context;
-}
-
-function traverseNode(node: any, context) {
-  const nodeTransforms = context.nodeTransforms;
-  for (let i = 0; i < nodeTransforms.length; i++) {
-    const transform = nodeTransforms[i];
-    transform(node);
+    nodeTransforms: options.nodeTransforms || []
   }
 
-  traverseChildren(node, context);
+  return context
 }
-function traverseChildren(node: any, context: any) {
-  const children = node.children;
+
+function traverseNode (node: any, context) {
+  const nodeTransforms = context.nodeTransforms
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i]
+    transform(node)
+  }
+
+  traverseChildren(node, context)
+}
+function traverseChildren (node: any, context: any) {
+  const children = node.children
 
   if (children) {
     for (let i = 0; i < children.length; i++) {
-      const node = children[i];
-      traverseNode(node, context);
+      const node = children[i]
+      traverseNode(node, context)
     }
   }
 }
